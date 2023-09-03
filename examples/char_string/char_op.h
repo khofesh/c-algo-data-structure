ed75165bb4d2825f1d9a8ee08f4d24e622363aba
fahmi ahmad <fahmiahmad@tutanota.com>
2023-09-03 16:21:05 +0700

    declare in header file

--- examples/char_string/char_op.h
+++ examples/char_string/char_op.h
@@ -1,6 +1,12 @@
 #ifndef CHAR_OP_H_
 #define CHAR_OP_H_
 
-
+int is_digit(char c);
+int is_lower(char c);
+int is_upper(char c);
+int is_whitespace(char c);
+int strlen(char *pStr);
+void to_upper(char *pStr);
+void to_lower(char *pStr);
 
 #endif /* CHAR_OP_H_ */
